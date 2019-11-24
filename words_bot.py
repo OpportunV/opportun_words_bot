3567ea823d3429e0fae28527f63a00c064e1a459
OpportunV <Vadya.95@mail.ru>
2019-11-24 14:18:59 +0300

    init

---
+++ words_bot.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+import telebot as tb
+import googletrans as gt
+
+
+def translate(phrase: str, source='en', target='ru') -> str:
+    return ''
+
+
+def get_rand_urban_word() -> Tuple[str, str]:
+    return '', ''
+
+
+def add_to_to_learn(word, meaning) -> None:
+    pass
+    
+
+def del_from_to_learn(word, meaning) -> None:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
