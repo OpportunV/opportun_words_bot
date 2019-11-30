c7bb4dfb73ad5d65191fd914e7411c39cbe2e456
OpportunV <Vadya.95@mail.ru>
2019-11-30 19:32:31 +0300

    simple functionality implemented with not simple bugs of course

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+from flask_sslify import SSLify
 from words_bot import init_bot_app
 import config
 
 
 bot, app = init_bot_app(config.TOKEN, config.URL)
+ssl = SSLify(app)
 
 
 if __name__ == '__main__':
